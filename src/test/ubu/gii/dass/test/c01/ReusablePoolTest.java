7e92c2ceac2a6e6c6fbab52293f50ae841aa932c
Saizcharly <83606181+saizchrly@users.noreply.github.com>
2024-02-29 19:30:28 +0100

    Update testReleaseRusable
    Acabamos la función de los tests public void testReleaseReusable

--- src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
+++ src/test/ubu/gii/dass/test/c01/ReusablePoolTest.java
@@ -9,6 +9,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ubu.gii.dass.c01.DuplicatedInstanceException;
+import ubu.gii.dass.c01.NotFreeInstanceException;
 import ubu.gii.dass.c01.Reusable;
 import ubu.gii.dass.c01.ReusablePool;
 
@@ -58,7 +60,7 @@ public class ReusablePoolTest {
 
             // Verificar que el objeto reusable adquirido no es nulo
             assertNotNull("El objeto reusable adquirido no debería ser nulo", reusable);
-        } catch (Exception e) {
+        } catch (NotFreeInstanceException e) {
             // Si hay una excepción, imprimir un mensaje de error
             fail("No se debería lanzar una excepción al adquirir un objeto reusable del pool");
         }
@@ -69,6 +71,28 @@ public class ReusablePoolTest {
 	 */
 	@Test
 	public void testReleaseReusable() {
+		// Crear una instancia del pool
+        ReusablePool pool = ReusablePool.getInstance();
+
+        // Crear un objeto Reusable
+        Reusable reusable = new Reusable();
+
+        try {
+            // Liberar el objeto Reusable en el pool
+            pool.releaseReusable(reusable);
+
+            // Intentar adquirir un objeto del pool
+            Reusable acquiredReusable = pool.acquireReusable();
+
+            // Verificar si el objeto adquirido es el mismo que el objeto liberado
+            assertEquals("El objeto adquirido debe ser el mismo que el objeto liberado", reusable, acquiredReusable);
+        } catch (DuplicatedInstanceException e) {
+            // Si se lanza una excepción, imprimir un mensaje de error
+            fail("No se debería lanzar una excepción al liberar un objeto reusable en el pool");
+        } catch (NotFreeInstanceException e) {
+            // Si se lanza una excepción, imprimir un mensaje de error
+            fail("No se debería lanzar una excepción al adquirir un objeto reusable del pool");
+        }
 	}
 
 }
